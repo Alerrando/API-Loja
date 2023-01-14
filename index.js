50bac2489793477c7b6c38048ffe15c074cc7b8a
Alerrando <alerrando2@gmail.com>
2023-01-14 12:39:11 -0300

    Hello World

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");   
+})
+
 const PORT= 8000;
 app.listen(process.env.PORT || PORT, () => console.log("Server ON"));
