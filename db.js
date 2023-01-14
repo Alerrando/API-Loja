52a698eaa98b72667b125199093df8734f3e9b71
Alerrando <alerrando2@gmail.com>
2023-01-14 17:25:02 -0300

    linking API

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ export const PORT = process.env.PORT || 8000;
 
 
 export const db = mysql2.createPool({
-  host: process.env.host || "containers-us-west-17.railway.app",
-  port: process.env.port || "5716",
+  host: process.env.host || "containers-us-west-72.railway.app",
+  port: process.env.port || "7859",
   user: process.env.user || "root",
-  password: process.env.password || "ll8DYOTrBTMm8D5DYqx0",
+  password: process.env.password || "zgSjIph4OeebNgDktUN9",
   database: process.env.database || "railway",
 });
