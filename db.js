8a9375cc443ac446d8317daa570dda74bc1c00cc
Alerrando Breno <alerrando2@gmail.com>
2023-04-17 10:28:52 -0300

    password db change

--- db.js
+++ db.js
@@ -6,6 +6,6 @@ export const db = mysql2.createPool({
   host: process.env.host || "containers-us-west-72.railway.app",
   port: process.env.port || "7859",
   user: process.env.user || "root",
-  password: process.env.password || "tnKXVsogrw7XJlDt5cAr",
+  password: process.env.password || "HaHEPBBIjQmQkfulFmWZ",
   database: process.env.database || "railway",
 });
